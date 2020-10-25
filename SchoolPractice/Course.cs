089af248940fa80acbf5560f54163964320115db
Rachel <rachel.rolon@gmail.com>
2020-10-24 22:18:09 -0500

    part 3 done

--- SchoolPractice/Course.cs
+++ SchoolPractice/Course.cs
@@ -10,6 +10,12 @@ namespace SchoolPractice
         // atleast 3 fields
         // atleast 1 should be a dictionary or list
         // should use your student class
+        private Student newStudent;
+        private int creditHours;
+        private Dictionary<string, int> meetingRoom;
+        private string meetingTime;
+        private Teacher professor;
 
+        
     }
 }
